291c51ace391a0e609852b15ad71b88b75f67457
wangleibeckham <wangleibeckham@gmail.com>
2016-09-26 23:06:51 -0400

    update popup box

--- Assets/Scripts/Label.js
+++ Assets/Scripts/Label.js
@@ -1,26 +1,28 @@
 #pragma strict
+
 var isClicked: boolean;
 var textureToDisplay : Texture2D;
-
-// var movement : Movement;
+var title: String;
+var description: String;
+var back = "Back";
+var style : GUIStyle;
 // var c : UnityEngine.GameObject;
 
 function Start ()
 {
+	;
 	isClicked = false;
 	if (gameObject.GetComponent(Collider) == null)
 	{
 		gameObject.AddComponent(typeof(BoxCollider));
 	}
 
-	// c = GameObject.Find("Main Camera");
-	// movement = c.GetComponent(Movement);
 }
 
 function OnMouseDown()
 {
 	isClicked = !isClicked;
-	// movement.frozen = true;
+//	 movement.frozen = true;
 }
 
 //function OnMouseUp()
@@ -30,25 +32,41 @@ function OnMouseDown()
 
 function DoWindow0 (windowID : int)
 {
-	var Button = GUI.Button (Rect (10,30, 80, 20), "Back");
+	
+	GUI.Label ( 
+	Rect (10, 30, 
+				(textureToDisplay.width >Screen.width*2/3)? Screen.width*2/3:textureToDisplay.width, 
+				textureToDisplay.height
+		 ), textureToDisplay);
+
+	GUI.Label ( Rect (
+	(textureToDisplay.width >Screen.width*2/3)? Screen.width*2/3+30:textureToDisplay.width+30,
+	30,
+	Screen.width/3,
+	textureToDisplay.height), description);
+	var Button = GUI.Button (Rect (10,textureToDisplay.height+50, 100, 50), back);
 	if(Button)
 	{
-		isClicked = false;
+		
+		isClicked = !isClicked;
 	}
 }
 
 function OnGUI()
 {
-	if (isClicked == true) {
-		GUI.Label ( Rect (10,10,200,20), "The Hidden Dinosaur Skeleton!");
-		GUI.Label ( Rect (10, 40, textureToDisplay.width, textureToDisplay.height), textureToDisplay);
-	}
+//	if (isClicked == true) {
+//		GUI.Label ( Rect (10,10,200,20), "The Hidden Dinosaur Skeleton!");
+//		GUI.Label ( Rect (10, 40, textureToDisplay.width, textureToDisplay.height), textureToDisplay);
+//	}
 
 	// Make a toggle button for hiding and showing the window
 	// isClicked = GUI.Toggle (Rect (10,10,100,20), isClicked, "MENU");
 	// Make sure we only call GUI.Window if doWindow0 is true.
 	if (isClicked)
 	{
-		GUI.ModalWindow (0, Rect (11,10,200,60), DoWindow0, "Basic Window");
+		GUI.backgroundColor=Color.grey;
+		GUI.ModalWindow (0, Rect (0,0,Screen.width,Screen.height), DoWindow0, title,style);
+		style.fontSize = 20;
+		style.alignment = TextAnchor.UpperCenter;
 	}
 }
