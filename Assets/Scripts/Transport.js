0331189bfadb3a300c680fde3f3bc396a6a6c79f
Kammy <liuk@live.unc.edu>
2016-10-22 18:40:09 -0400

    add documentation comments for Transport.js

--- Assets/Scripts/Transport.js
+++ Assets/Scripts/Transport.js
@@ -1,17 +1,29 @@
-﻿#pragma strict
+﻿/**
+ * Handles scene changes when attached to a game object<br>
+ * Kammy Liu
+ *
+ * @class Transport
+ * @extends MonoBehavior
+ */
 
+#pragma strict
+
+/**
+* Name of the scene to switch to
+*
+* @property sceneName
+* @type String
+*/
 public var sceneName : String;
 
+/**
+ * Inherited from MonoBehavior. Called on each frame the mouse is over the object.
+ * If the left mouse is down, changes the scene to the one specified by "sceneName"
+ *
+ * @method OnMouseOver
+ */
 function OnMouseOver(){
 	if(Input.GetMouseButtonDown(0)){
     	SceneManagement.SceneManager.LoadScene(sceneName);
 	}
-}
-
-function Start () {
-
-}
-
-function Update () {
-
 }
